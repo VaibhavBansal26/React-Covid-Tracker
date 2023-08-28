4340b2c6834de71036e4f5c82f0a45f16620eccc
VaibhavBansal26 <vaibhav.bansal945@gmail.com>
2023-08-29 01:12:58 +0530

    covid tracker

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCWJMBH4xe72bR5n7BGVXQrakyoyXO8SwE",
+    authDomain: "react-covid-vb.firebaseapp.com",
+    projectId: "react-covid-vb",
+    storageBucket: "react-covid-vb.appspot.com",
+    messagingSenderId: "810239763608",
+    appId: "1:810239763608:web:0abbf27b7a78b57b12dced"
+  };
